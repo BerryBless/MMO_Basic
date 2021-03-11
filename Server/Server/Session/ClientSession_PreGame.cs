c8d1ad78394dca21b335665c1799c00461a1c69a
BerryBless <shsuzakfn@gmail.com>
2021-03-11 21:04:06 +0900

    HandleLogin DB연동2
    로그인하면 계정이름 조회해서 캐릭터 리스트 불러오기

--- Server/Server/Session/ClientSession_PreGame.cs
+++ Server/Server/Session/ClientSession_PreGame.cs
@@ -11,15 +11,19 @@ namespace Server
 {
     public partial class ClientSession : PacketSession
     {
+        public int AccountDbId { get; private set; }
+        public List<LobbyPlayerInfo> LobbyPlayers { get; set; } = new List<LobbyPlayerInfo>();
+
         public void HandleLogin(C_Login loginPacket)
         {
             // TODO 보안체크 더 강하게
             if (ServerState != PlayerServerState.ServerStateLogin) return;
-                 
+
             // TODO 각종 상황에 대비
             // -동시?
             // -악질 패킷
             // -이상한 타이밍
+            LobbyPlayers.Clear();
             using (AppDbContext db = new AppDbContext())
             {
                 AccountDb findAccount = db.Accounts
@@ -29,8 +33,33 @@ namespace Server
 
                 if (findAccount != null)
                 { // 로그인 성공
+                    AccountDbId = findAccount.AccountId;        // Id는 자주쓰니 기억
                     S_Login loginOk = new S_Login() { LoginOk = 1 };
+                    foreach(PlayerDb playerDb in findAccount.Players)
+                    {
+                        LobbyPlayerInfo lobbyPlayer = new LobbyPlayerInfo() {
+                            Name = playerDb.PlayerName,
+                            StatInfo = new StatInfo()
+                            {
+                                Level = playerDb.Level,
+                                Hp = playerDb.Hp,
+                                MaxHp = playerDb.MaxHp,
+                                Attack = playerDb.Attack,
+                                Speed = playerDb.Speed,
+                                TotalExp = playerDb.TotalExp
+                            }
+                        };
+
+                        // 메모리에 들고있는다 DB접근 최소화
+                        LobbyPlayers.Add(lobbyPlayer);
+                        // 패킷을 넣어준다
+                        loginOk.Players.Add(lobbyPlayer);
+
+                    }
                     this.Send(loginOk);
+
+                    // 로비로 이동
+                    ServerState = PlayerServerState.ServerStateLobby;
                 }
                 else
                 { // 실패
@@ -39,9 +68,14 @@ namespace Server
                     db.Accounts.Add(newAccount);
                     db.SaveChanges();   // TODO : Exception
 
-                    // 로그인 성공
+                    AccountDbId = findAccount.AccountId;        // Id는 자주쓰니 기억
+
+                    // 로그인 실패
                     S_Login loginOk = new S_Login() { LoginOk = 1 };
                     this.Send(loginOk);
+
+                    // 로비로 이동
+                    ServerState = PlayerServerState.ServerStateLobby;
                 }
             }
         }
